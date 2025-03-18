599ddfd1842dee4be493db7ab89f727f0f3d156c
Chandan-pal-2004 <palchandan0805@gmail.com>
2025-03-18 21:50:52 +0530

    Coded Function Validate

--- config/function.php
+++ config/function.php
@@ -1,1 +1,13 @@
+<?php
+require 'db.php';
 
+function validate($inputData)
+{
+    global $conn;
+
+    $validatedData = mysqli_real_escape_string($conn, $inputData);
+    return trim($validatedData);
+
+}
+
+?>
