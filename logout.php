42603af46bf2cde1ce0bcfc6e36f52bd7d3302cd
Chandan-pal-2004 <palchandan0805@gmail.com>
2025-03-21 05:46:21 +0530

    prevent going to logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . '/ams/config/function.php');
 
-if(isset($_SESSION['auth']) || !isset($_SESSION['loggedInUserRole']) || !isset($_SESSION['loggedInUserId']) || !isset($_SESSION['loggedInUserName']) || !isset($_SESSION['loggedInUserEmail']))
+if(isset($_SESSION['auth']) || isset($_SESSION['loggedInUserRole']) || isset($_SESSION['loggedInUserId']) || isset($_SESSION['loggedInUserName']) || isset($_SESSION['loggedInUserEmail']))
 {
     logoutSession();
     redirect('/ams/login.php', 'Logout SuccessFully!.');
